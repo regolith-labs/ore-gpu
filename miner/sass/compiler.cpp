550cd387553f3d844fe28265c572ea934135dfb5
Hardhat Chad <hardhatchad@gmail.com>
2025-03-04 12:29:23 -0800

    fix

--- miner/sass/compiler.cpp
+++ miner/sass/compiler.cpp
@@ -3,11 +3,11 @@
 #include <Assembler/Assembler.h>
 
 HashXCompiler::HashXCompiler() : assembler() {
-    assembler.SetComputeCapability(70); // Volta (CC 7.0)
 }
 
 std::vector<uint8_t> HashXCompiler::compile(const hashx_program& prog) {
     auto sass_program = translate_hashx_to_sass(prog);
     
-    return assembler.Assemble(sass_program);
+    auto binary = assembler.Assemble(sass_program);
+    return binary->GetBytes();
 } 
