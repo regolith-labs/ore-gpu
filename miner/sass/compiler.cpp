14f5a78e971bcd7e34014d2290af69e254e6a046
Hardhat Chad <hardhatchad@gmail.com>
2025-03-04 12:14:15 -0800

    build

--- miner/sass/compiler.cpp
+++ miner/sass/compiler.cpp
@@ -2,16 +2,12 @@
 #include "translator.h"
 #include <Assembler/Assembler.h>
 
-struct HashXCompiler {
-    SASS::Volta::Assembler assembler;
+HashXCompiler::HashXCompiler() : assembler() {
+    assembler.SetComputeCapability(89); // Ada Lovelace (RTX 40-series)
+}
+
+std::vector<uint8_t> HashXCompiler::compile(const hashx_program& prog) {
+    auto sass_program = translate_hashx_to_sass(prog);
     
-    HashXCompiler() {
-        assembler.SetComputeCapability(89); // Ada Lovelace (RTX 40-series)
-    }
-    
-    std::vector<uint8_t> compile(const hashx_program& prog) {
-        auto sass_program = translate_hashx_to_sass(prog);
-        
-        return assembler.Assemble(sass_program);
-    }
-}; 
+    return assembler.Assemble(sass_program);
+} 
