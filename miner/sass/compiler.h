f42bf35ab41f0f7c48e02fb348bb2541471f1411
Hardhat Chad <hardhatchad@gmail.com>
2025-03-04 12:15:24 -0800

    build

--- miner/sass/compiler.h
+++ miner/sass/compiler.h
@@ -1,10 +1,14 @@
 #pragma once
 #include <vector>
 #include <cstdint>
+#include <SASS/Volta/Assembler.h>
 
 struct hashx_program;
 
 class HashXCompiler {
+private:
+    SASS::Volta::Assembler assembler;
+
 public:
     HashXCompiler();
     std::vector<uint8_t> compile(const hashx_program& prog);
